1ca2b798331281ca75758cd9dd0e2dae491600e2
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-12-26 10:09:05 -0500

    created server.test.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -14,13 +14,15 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-todo.save().then((doc) => {
-    res.send(doc);
-}, (e) => {
-    res.status(400).send(e);
-});
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = { app };
