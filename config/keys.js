3072bfa89e2ec611b268d4da68d74e572dcda33a
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-12-26 17:39:58 -0500

    testing

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod');
+    console.log('running in prod...')
 } else{
-    module.exports = require('./prod');
+    module.exports = require('./dev');
+    console.log('running in dev...')
 }
