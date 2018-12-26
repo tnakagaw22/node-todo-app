68da2c7119f4de0489ce7744471917b5ecaceb10
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-12-26 17:47:37 -0500

    set port correctly

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod');
-    console.log('running in prod...')
 } else{
     module.exports = require('./dev');
-    console.log('running in dev...')
 }
