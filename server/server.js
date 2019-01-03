23c433e90e4d88463978725c2a5b5cc19ee8d588
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-01-02 20:26:10 -0500

    added user/me for the first private route

--- server/server.js
+++ server/server.js
@@ -110,6 +110,21 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users/me', (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            // catch will handle it.
+            return Promise.reject();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
