3e32de840cd549079ef9b35a2603e11260d68127
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-01-05 08:41:46 -0500

    added login route

--- server/server.js
+++ server/server.js
@@ -115,6 +115,19 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        // return will keep the chain alive so that it can catch any errors
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
